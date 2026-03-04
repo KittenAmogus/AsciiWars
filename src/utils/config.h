c33e9a8987cc8f1aa1af2a3fdb7d0a2ac274ccd8
KittenAmogus <kittenamogus3@gmail.com>
2026-03-04 12:46:39 +0300

    Add int types in config

--- src/utils/config.h
+++ src/utils/config.h
@@ -1,4 +1,27 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// For compilation
+#define ARDUINO 0
+#define DEBUG 0
+
+// Basic types
+#ifndef uint8_t
+#define uint8_t unsigned char
+#endif
+
+#ifndef uint16_t
+#define uint16_t unsigned short
+#endif
+
+#ifndef int8_t
+#define int8_t signed char
+#endif
+
+#ifndef int16_t
+#define int16_t signed short
+#endif
+
+// If u see it, send me in char '0xA0', i want to check if u seen my code
+
 #endif // CONFIG_H
